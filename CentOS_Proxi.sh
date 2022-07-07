c40b2ae1adb0ca8f80f02584a896fa79c9528970
canerd7u <cnr191@hotmail.com>
2022-07-07 13:51:59 +0300

    Update CentOS_Proxi.sh

--- CentOS_Proxi.sh
+++ CentOS_Proxi.sh
@@ -76,11 +76,13 @@ EOF
 config_3proxy() {
     cat <<EOF
 daemon
-maxconn 1000
-nscache 65536
+nserver 77.88.8.8
+nserver 77.88.8.1
+nscache 90000
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
+stacksize 90000
 flush
 auth strong
 
